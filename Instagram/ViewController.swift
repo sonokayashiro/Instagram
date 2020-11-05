86b1096a8b53e5830bb0c41a960e41e8a8a87ace
sonokayashiro <scsshyashiro@gmail.com>
2020-11-05 16:16:24 +0900

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by 家城苑佳 on 2020/11/05.
+//  Copyright © 2020 sonoka.yashiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
